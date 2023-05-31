6c63961edf503916068fbdf1a53b82b4c3b2f059
Alex Sanders <alex@sndrs.dev>
2023-05-31 17:44:25 +0100

    hit tq

--- simple_api.ts
+++ simple_api.ts
@@ -1,3 +1,13 @@
-import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.190.0/http/mod.ts";
 
 serve((req: Request) => new Response("Hello World"));
+async function reqHandler(req: Request) {
+	const reqPath = new URL(req.url).pathname;
+	const backendResponse = await fetch("https://thequietus.com" + reqPath, {
+		headers: req.headers,
+	});
+
+	return backendResponse;
+}
+
+serve(reqHandler, { port: 8000 });
