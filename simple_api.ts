02eb6f6fe44186b6fc8746339e8f16a56f05f05c
Alex Sanders <alex@sndrs.dev>
2023-05-31 17:50:02 +0100

    Update simple_api.ts

--- simple_api.ts
+++ simple_api.ts
@@ -2,9 +2,11 @@ import { serve } from "https://deno.land/std@0.190.0/http/mod.ts";
 
 async function reqHandler(req: Request) {
 	const reqPath = new URL(req.url).pathname;
+	console.time(`fetch ${reqPath}`);
 	const backendResponse = await fetch("https://thequietus.com" + reqPath, {
 		headers: req.headers,
 	});
+	console.timeEnd(`fetch ${reqPath}`);
 
 	return backendResponse;
 }
